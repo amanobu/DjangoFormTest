d6b55acad433694fd184ab81f2c2dacbb8535897
amanobu <nqf33359@nifty.com>
2017-10-06 02:13:01 +0000

    check input data

--- SignupForm/forms.py
+++ SignupForm/forms.py
@@ -5,8 +5,10 @@ class NameForm(forms.Form):
     your_name = forms.CharField(label = 'お名前は？' ,max_length=100)
 
     def clean(self):
+        #cleand_dataには入力チェックが通った物が入っている
         name = self.cleaned_data['your_name']
         if name.find('テスト') == -1:
             raise forms.ValidationError("入力エラー。「テスト」って入ってないよ")
-        return name
+        else:
+            return name
 
